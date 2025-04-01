9797bc5f98f4482291b92da949a7f98ad3e0a474
Luna Vulperya <lunavulperya@gmail.com>
2025-03-31 21:40:36 -0300

    implementa uso de Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("androidx.room:room-rutime:2.6.1")
+
+    implementation("androidx.room:room-compiler:2.6.1")
 }
